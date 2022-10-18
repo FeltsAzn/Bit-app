f54928fbe7d0d23af1f31063fda6d5d122ba688a
Timur <107147438+FeltsAzn@users.noreply.github.com>
2022-10-18 18:10:55 +0300

    Создание сессии клиента, для автоматического подтягивания заголовков

--- tg_bot/client.py
+++ tg_bot/client.py
@@ -1,36 +1,24 @@
-from tg_bot.tg_bot_config import API_URL, SECRET_HEADER
+from tg_bot.tg_bot_config import API_URL, SECRET_HEADER,  ADMIN_TOKEN, ADMIN_PASSWORD, ADMIN_ID
 import requests
 from fastapi_app import schemas
-from tg_bot.tg_bot_config import ADMIN_TOKEN
-
-users_token_cache = {}
 
 
-def get_users():
-    """
-    Получаем всех юзеров (для админов)
-    :return server response(list):
-    """
-    try:
-        response = requests.get(f"{API_URL}/users",
-                                headers={"Authorization": f'Bearer {ADMIN_TOKEN}'}).json()
-    except requests.exceptions.ConnectionError as _ex:
-        raise _ex
-    return response
+form_headers = {'Content-Type': 'application/x-www-form-urlencoded'}
 
-
-def get_info_about_user(user_id: int):
-    """
-    Получение информации о пользователе (для админа)
-    :param user_id:
-    :return server response(dict):
-    """
-    try:
-        response = requests.get(f'{API_URL}/get_info_by_user/{user_id}',
-                                headers={"Authorization": f'Bearer {ADMIN_TOKEN}'}).json()
-    except requests.exceptions.ConnectionError as _ex:
-        return {"server_error": "the server is not responding"}
-    return response
+# когда мы отправляем данные в виде формы,
+# то присваиваем значения параметрам через равно,
+# а перечисляем их через амперсанд
+payload = f'username={ADMIN_ID[0]}&password={ADMIN_PASSWORD}'
+raw_token = requests.post(API_URL+"/token",
+                          headers=form_headers,
+                          data=payload)
+token = raw_token.json()     # получаем словарь из ответа сервера
+session = requests.Session()   # создаем экземпляр сессии
+# добавляем хедеры с токеном авторизации, благодаря чему API будет понимать кто мы и возвращать нужные нам ответы
+session.headers = {
+      'accept': 'application/json',
+      'Authorization': "Bearer " + token['access_token']
+}
 
 
 def get_user_by_tg(tg_id: int):
@@ -63,66 +51,6 @@ def create_user(user: dict):
     return response
 
 
-def update_user(user: schemas.UserUpdate) -> str | dict:
-    """
-    Обновление информации пользователя (для админа)
-    :param user:
-    :return server response(dict | str) :
-    """
-    try:
-        user = schemas.UserUpdate.validate(user)
-        response = requests.put(f'{API_URL}/user/{user.id}', data=user.json(),
-                                headers={"Authorization": f'Bearer {ADMIN_TOKEN}'})
-    except requests.exceptions.ConnectionError as _ex:
-        return {"server_error": "the server is not responding"}
-    else:
-        try:
-            return response.json()
-        except Exception as ex:
-            return response.text
-
-
-def delete_user(user_id: int) -> dict:
-    """
-    Удаление пользователя (для админа)
-    :param user_id:
-    :return server response(dict):
-    """
-    try:
-        response = requests.delete(f'{API_URL}/user/{user_id}',
-                                   headers={"Authorization": f'Bearer {ADMIN_TOKEN}'}).json()
-    except requests.exceptions.ConnectionError as _ex:
-        return {"server_error": "the server is not responding"}
-    return response
-
-
-def get_user_balance(tg_id: int) -> dict:
-    """
-    Получение баланса пользователя
-    :param tg_id:
-    :return server response(dict):
-    """
-    try:
-        response = requests.get(f"{API_URL}/get_user_balance/{tg_id}",
-                                headers={"secret-key": SECRET_HEADER}).json()
-    except requests.exceptions.ConnectionError as _ex:
-        return {"server_error": "the server is not responding"}
-    return response
-
-
-def total_balance() -> dict:
-    """
-    Получение общего баланса пользователей (для админа)
-    :return server response(dict):
-    """
-    try:
-        response = requests.get(f"{API_URL}/get_total_balance",
-                                headers={"Authorization": f'Bearer {ADMIN_TOKEN}'}).json()
-    except requests.exceptions.ConnectionError as _ex:
-        return {"server_error": "the server is not responding"}
-    return response
-
-
 def create_transaction(transaction_info: dict) -> dict:
     """
     Создание транзакции
@@ -148,6 +76,88 @@ def get_user_transactions(tg_id: int) -> dict:
     try:
         response = requests.get(f"{API_URL}/get_user_transactions/{tg_id}",
                                 headers={"secret-key": SECRET_HEADER}).json()
+    except requests.exceptions.ConnectionError as _ex:
+        return {"server_error": "the server is not responding"}
+    return response
+
+
+def get_user_balance(tg_id: int) -> dict:
+    """
+    Получение баланса пользователя
+    :param tg_id:
+    :return server response(dict):
+    """
+    try:
+        response = requests.get(f"{API_URL}/get_user_balance/{tg_id}",
+                                headers={"secret-key": SECRET_HEADER}).json()
+    except requests.exceptions.ConnectionError as _ex:
+        return {"server_error": "the server is not responding"}
+    return response
+
+
+def get_users():
+    """
+    Получаем всех юзеров (для админов)
+    :return server response(list):
+    """
+    try:
+        response = session.get(f"{API_URL}/users").json()
+    except requests.exceptions.ConnectionError as _ex:
+        raise _ex
+    return response
+
+
+def get_info_about_user(user_id: int):
+    """
+    Получение информации о пользователе (для админа)
+    :param user_id:
+    :return server response(dict):
+    """
+    try:
+        response = session.get(f'{API_URL}/get_info_by_user/{user_id}').json()
+    except requests.exceptions.ConnectionError as _ex:
+        return {"server_error": "the server is not responding"}
+    return response
+
+
+def update_user(user: schemas.UserUpdate) -> str | dict:
+    """
+    Обновление информации пользователя (для админа)
+    :param user:
+    :return server response(dict | str) :
+    """
+    try:
+        user = schemas.UserUpdate.validate(user)
+        response = session.put(f'{API_URL}/user/{user.id}', data=user.json())
+    except requests.exceptions.ConnectionError as _ex:
+        return {"server_error": "the server is not responding"}
+    else:
+        try:
+            return response.json()
+        except Exception as ex:
+            return response.text
+
+
+def delete_user(user_id: int) -> dict:
+    """
+    Удаление пользователя (для админа)
+    :param user_id:
+    :return server response(dict):
+    """
+    try:
+        response = session.delete(f'{API_URL}/user/{user_id}').json()
+    except requests.exceptions.ConnectionError as _ex:
+        return {"server_error": "the server is not responding"}
+    return response
+
+
+def total_balance() -> dict:
+    """
+    Получение общего баланса пользователей (для админа)
+    :return server response(dict):
+    """
+    try:
+        response = session.get(f"{API_URL}/get_total_balance").json()
     except requests.exceptions.ConnectionError as _ex:
         return {"server_error": "the server is not responding"}
     return response
